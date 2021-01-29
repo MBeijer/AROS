4988554a87d86ad00f3dc67159480e544b2980cc
Marlon Beijer <marlon@eevul.org>
2021-01-29 04:21:07 +0100

    Revert "First phase of the the cleanup:  Much of the driver and architecture stuff which is non-native to M68K is removed."
    This reverts commit 04979e6b47deb63932cf85f5664426092868c60b.

---
+++ arch/all-hosted/hidd/x11/x11_debug.h
@@ -0,0 +1,9 @@
+/*
+    Copyright © 2014, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#include <aros/debug.h>
---
+++ arch/arm-native/kernel/kernel_fb.h
@@ -0,0 +1,11 @@
+/*
+    Copyright © 2013, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#if !defined(KRN_Dummy)
+#define KRN_Dummy               (TAG_USER + 0x03d00000)
+#endif
+#if !defined(KRN_FuncPutC)
+#define KRN_FuncPutC      (KRN_Dummy + 99) /* RAW FrameBuffer descriptor */
+#endif
